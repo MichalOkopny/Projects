31c3e94fb7bfab912cea265fc8bb1927c893e4f8
MichalOkopny <131303933+MichalOkopny@users.noreply.github.com>
2024-09-13 16:40:00 +0200

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -6,16 +6,15 @@ import requests
 from datetime import datetime, timedelta
 
 def fetch_data():
-    # Przykładowy kod dla API
     api_url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true"
     headers = {"Authorization": "Bearer CG-XNtTAs2nibEmDABbeYcfmitB"}
     
     try:
         response = requests.get(api_url, headers=headers)
-        response.raise_for_status()  # Sprawdź, czy zapytanie było udane
-        data = response.json()  # Przekształć dane do formatu JSON
-        df = pd.DataFrame(data)  # Konwertuj do DataFrame
+        response.raise_for_status()  
+        data = response.json()  
+        df = pd.DataFrame(data) 
         return df
     except Exception as e:
         print(f"Error fetching data: {e}")
-        return pd.DataFrame()  # Lub inny sposób obsługi błędów
+        return pd.DataFrame()  
